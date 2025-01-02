dcc89a44b14d24c4aa9505a5139fc7a9507150db
setbs <kr9ka41@gmail.com>
2025-01-02 23:08:36 +0200

    Product filter on the site page and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from django.utils.http import urlencode
 from goods.models import Categories
 from django import template
 register = template.Library()
@@ -8,4 +9,8 @@ register = template.Library()
 def tag_categories():
     return Categories.objects.all() 
  
-    
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs): 
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -6,13 +6,23 @@ from django.core.paginator import Paginator
 def catalog(request, category_slug, page=1):
     
     page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
     
     if category_slug == "all":
         goods = Products.objects.all()
     else: 
         goods =  get_list_or_404(Products.objects.filter(category__slug=category_slug)) 
 
-# Paginanion
+    if on_sale: 
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default": 
+        goods = goods.order_by(order_by)
+    
+
+
+# Pagination
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page)) 
     
