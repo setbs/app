f44df265302fe97245221758b6aeb3fa558c13a4
setbs <kr9ka41@gmail.com>
2025-01-22 05:46:15 +0200

    account creating and login done

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.http import  HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import auth, messages
 from django.urls import reverse
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def login(request): 
@@ -27,8 +27,20 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():   
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+
+    else: 
+        form = UserRegistrationForm()
+    
     context = {
-        '': ''
+        'title': 'Home - Registration', 
+        'form' : form
     }
     return render(request, 'users/registration.html', context)
     
@@ -38,4 +50,5 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 def logout(request): 
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
