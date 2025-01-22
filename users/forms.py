bd497bbdd2d4ef24785af3d5a47980c618871a1d
setbs <kr9ka41@gmail.com>
2025-01-22 04:37:34 +0200

    Login form and controller with notifications done

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from users.models import User
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label = 'Name', 
+        widget=forms.TextInput(attrs={"autofocus" : True,
+                                      'class':'form-control',
+                                      "placeholder":"Enter username", 
+                                      }))
+    password = forms.CharField(
+        label = 'Password', 
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+                                          'class':'form-control',
+                                          'placeholder':'Enter your password'})
+    )
+    class Meta: 
+        model = User    
--- users/views.py
+++ users/views.py
@@ -1,9 +1,28 @@
+from django.http import  HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth, messages
+from django.urls import reverse
+from users.forms import UserLoginForm
 
 # Create your views here.
 def login(request): 
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user= auth.authenticate(username=username, password=password)
+            if user: 
+                messages.success(request, f"You logged into {username} successfull")
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+
+    else: 
+        form = UserLoginForm()
+    
     context = {
-        'title' : ''
+        'title' : 'Home - Authorization',
+        'form' : form,
     }
     return render(request, 'users/login.html', context)
 
